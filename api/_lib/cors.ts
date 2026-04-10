1593eba6d91525128c0ed368b759fdaf3d0a145f
PanParampampam <giedrysjakub@gmail.com>
2026-04-10 20:05:11 +0200

    Update cors.ts

--- api/_lib/cors.ts
+++ api/_lib/cors.ts
@@ -5,9 +5,13 @@ export function setCorsHeaders(req, res) {
     "https://wedding-planner-4404zm5jx-kubas-projects-640afcef.vercel.app",
   ];
   const origin = req.headers.origin;
+  console.log("CORS request from origin:", origin);
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
+  } else {
+    res.setHeader("Access-Control-Allow-Origin", "");
   }
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
 }
