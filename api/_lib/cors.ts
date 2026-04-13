046dee28d1d869130255ad7a803741eef76dd66f
PanParampampam <giedrysjakub@gmail.com>
2026-04-13 15:35:41 +0200

    Typing fix

--- api/_lib/cors.ts
+++ api/_lib/cors.ts
@@ -1,4 +1,12 @@
-export function setCorsHeaders(req, res) {
+//Todo: typing fix
+
+import type { Http2ServerRequest } from "http2";
+import type { Http2ServerResponse } from "http2";
+
+export function setCorsHeaders(
+  req: Http2ServerRequest,
+  res: Http2ServerResponse,
+) {
   const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:3000",
@@ -7,7 +15,7 @@ export function setCorsHeaders(req, res) {
   const origin = req.headers.origin;
   console.log("CORS request from origin:", origin);
   if (
-    allowedOrigins.includes(origin) ||
+    (origin && allowedOrigins.includes(origin)) ||
     (origin &&
       origin.startsWith("https://wedding-planner-") &&
       origin.endsWith(".vercel.app"))
--- api/guests/index.ts
+++ api/guests/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+//TODO: FIX THE TYPING
 import { prisma } from "../_lib/prisma.js";
 import { setCorsHeaders } from "../_lib/cors.js";
 import { Prisma } from "../../src/generated/prisma/client.js";
@@ -7,22 +9,17 @@ export type createGuestResponse = {
   message: string;
 };
 
-export default async function handler(req, res) {
-  // Set CORS headers for all responses
-  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  // res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
+export default async function handler(req: any, res: any): Promise<unknown> {
   setCorsHeaders(req, res);
 
   // Handle preflight OPTIONS request
-  if (req.method === "OPTIONS") {
-    return res.status(204).end();
-  }
+  // if (req.method === "OPTIONS") {
+  //   return res.status(204).end();
+  // }
 
   if (req.method === "GET") {
     const guests = await prisma.guest.findMany();
-    return res.status(200).json(guests);
+    return res.json(guests);
   }
 
   if (req.method === "POST") {
